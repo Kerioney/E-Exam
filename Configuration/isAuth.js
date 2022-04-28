97562909333eb0f989a32d31fd8b43f98f10dfd2
Kerioney <Fareselkerioney@gmail.com>
2022-04-28 16:56:43 +0200

    Admin Controller

--- Configuration/isAuth.js
+++ Configuration/isAuth.js
@@ -1,26 +1,25 @@
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 // auth schema
-const rbac = require('../Auth/RBAC/rbac');
+const rbac = require('../Auth/RBAC/rbac')
 // // we get the rbc that we created
 
 module.exports = (endpoint) => {
     //the parameter that will take it in the function
     return async (req, res, next) => {
-        let bareToken = req.headers.authorization;
+        let bareToken = req.headers.authorization
 
-        let token = bareToken.split(' ')[1];
+        let token = bareToken.split(' ')[1]
 
-        let decoded = jwt.verify(token, 'HHH');
+        let decoded = jwt.verify(token, 'HHH')
 
-        const isAllowed = await rbac.can(decoded.role, endpoint);
-        req.user = decoded;
+        const isAllowed = await rbac.can(decoded.role, endpoint)
+        req.user = decoded
 
-        // console.log(isAllowed);
         if (isAllowed) {
-            next();
+            next()
         } else {
-            res.status(401).json({ message: "You aren't authorized" });
+            res.status(401).json({ message: "You aren't authorized" })
             //401 unauthorized
         }
-    };
-};
+    }
+}
