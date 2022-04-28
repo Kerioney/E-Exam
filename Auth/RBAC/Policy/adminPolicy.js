c48751bf7f361b2c13a8dee717068d3d7a6782ad
Kerioney <Fareselkerioney@gmail.com>
2022-04-28 18:35:03 +0200

    Admin: Profs UD

--- Auth/RBAC/Policy/adminPolicy.js
+++ Auth/RBAC/Policy/adminPolicy.js
@@ -1,8 +1,10 @@
 const {
     GET_ALL_STUDENT,
-    GET_ALL_PROFESSOR,
     UPDATE_STUDENT,
     DELETE_STUDENT,
+    GET_ALL_PROFESSOR,
+    UPDATE_PROFESSOR,
+    DELETE_PROFESSOR,
 } = require('../../endpoints')
 
 module.exports = [
@@ -10,4 +12,6 @@ module.exports = [
     GET_ALL_PROFESSOR,
     UPDATE_STUDENT,
     DELETE_STUDENT,
+    UPDATE_PROFESSOR,
+    DELETE_PROFESSOR,
 ]
