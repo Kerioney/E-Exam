d5ce439ff29d9630c3353c8c3ab2e5abf9e7c6fa
Fares El-Kerioney <Fareselkerioney@gmail.com>
2023-05-02 19:06:08 +0300

    Secure DB,Headers

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 // Global Modules:
-const express = require('express')
-const connection = require('./Configuration/config')
+const express = require("express")
+const connection = require("./Configuration/config")
 const app = express()
-const bp = require('body-parser')
-const cors = require('cors')
+const bp = require("body-parser")
+const helmet = require("helmet")
+const mongoSanitize = require("express-mongo-sanitize")
+const cors = require("cors")
 
 //local routes:
-const Students = require('./Modules/Students/Routes/students.routes')
-const Professors = require('./Modules/Professors/Routes/professor.routes')
-const Admin = require('./Modules/Admin/Routes/admin.routes')
-const Exams = require('./Modules/Exams/Routes/exam.routes')
-const Questions = require('./Modules/Questions/Routes/question.routes')
+const Students = require("./Modules/Students/Routes/students.routes")
+const Professors = require("./Modules/Professors/Routes/professor.routes")
+const Admin = require("./Modules/Admin/Routes/admin.routes")
+const Exams = require("./Modules/Exams/Routes/exam.routes")
+const Questions = require("./Modules/Questions/Routes/question.routes")
 
 //middleware:
 app.use(bp.json())
-app.use(bp.urlencoded({ extended: false }))
+app.use(helmet())
+app.use(mongoSanitize())
 app.use(Students)
 app.use(Professors)
 app.use(Admin)
@@ -22,8 +25,5 @@ app.use(Exams)
 app.use(Questions)
 app.use(cors())
 
-app.get('/', (req, res) => res.send('Api is working....'))
-app.listen(process.env.PORT, () => console.log(`Server is running ........`))
-
-connection() //db Connection
-require('dotenv').config() //.env
+connection(app) //db Connection
+require("dotenv").config() //.env
