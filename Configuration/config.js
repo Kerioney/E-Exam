58ead66ae91aa97b87e7c2c393b1641bafc58686
Fares El-Kerioney <Fareselkerioney@gmail.com>
2023-05-02 19:06:48 +0300

    DB Config

--- Configuration/config.js
+++ Configuration/config.js
@@ -1,9 +1,23 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose")
+require("dotenv").config()
 
-const connection = () => {
-    return mongoose
-        .connect(process.env.CONNECTION_STRING)
-        .then(() => console.log('DB is Connecting.......'));
-};
-module.exports = connection;
+const connection = (app) => {
+    try {
+        mongoose
+            .set("strictQuery", false)
+            .connect(process.env.CONNECTION_STRING, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            })
+        app.listen(process.env.PORT || 3000, () =>
+            console.log(`Server is connecting on Port ${process.env.PORT}`)
+        )
+    } catch (err) {
+        if (!err.statusCode) {
+            err.statusCode = 503
+        }
+        throw err
+    }
+}
+
+module.exports = connection
